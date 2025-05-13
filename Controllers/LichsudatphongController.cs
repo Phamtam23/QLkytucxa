00bebb15042257d17ec828aeb063f88e48a29eb8
Phamtam23 <phamthientam20041@gmail.com>
2025-05-14 01:09:27 +0700

    add view lichsudatphong

---
+++ Controllers/LichsudatphongController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quanlykytucxa.Controllers
+{
+    public class LichsudatphongController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DetailLsdatphong ()
+        {
+            return View();
+        }
+    }
+}

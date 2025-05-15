5d3f39f0685c8e835ffa6ef0772e748502cf2ea1
Phamtam23 <phamthientam20041@gmail.com>
2025-05-16 00:28:03 +0700

    add view account

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Quanlykytucxa.Controllers
 {
@@ -24,6 +26,7 @@ namespace Quanlykytucxa.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();

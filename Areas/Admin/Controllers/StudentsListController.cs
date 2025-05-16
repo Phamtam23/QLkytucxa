69ac216a629ac71bded6c719cd866e0d59fc721b
VanHuy221 <23115053122221@sv.ute.udn.vn>
2025-05-16 17:55:44 +0700

    add danh sach sinh vien

---
+++ Areas/Admin/Controllers/StudentsListController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Quanlykytucxa.Models;
+using System.Linq;
+
+namespace Quanlykytucxa.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class StudentsListController : Controller
+    {
+        /*private readonly QuanLyKTXContext _context;
+
+        public StudentsListController(QuanLyKTXContext context)
+        {
+            _context = context;
+        }*/
+
+        public IActionResult Index()
+        {
+            /*var listSinhVien = _context.Users.OfType<SinhVien>().ToList();*/
+            return View(/*listSinhVien*/);
+        }
+    }
+}

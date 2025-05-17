8dcdd71226a8796bcc65a4da47132bc29ba6c321
VanHuy221 <23115053122221@sv.ute.udn.vn>
2025-05-17 09:45:06 +0700

    add trang phan bo phong

---
+++ Areas/Admin/Controllers/ArrangeRoomController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quanlykytucxa.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArrangeRoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

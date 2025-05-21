dd4c23fcb9aa1f50c58c36da7f52efc405d48d90
VanHuy221 <23115053122221@sv.ute.udn.vn>
2025-05-22 00:42:33 +0700

    add trang Thong Bao

---
+++ Models/SendNotificationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quanlykytucxa.Models.ViewModels
+{
+    public class SendNotificationViewModel
+    {
+        public string SinhVienId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TieuDe { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string NoiDung { get; set; }
+
+        public SelectList SinhViens { get; set; } // Dùng để bind dropdown
+    }
+}
